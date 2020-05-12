d8150a5b18f566749cdbf7d0d9a14e651ea6227a
FlightLoong <1577886278@qq.com>
2020-05-12 23:59:28 +0800

    todolist控制全选按钮

--- src/003 - todomvc/views/Todos.jsx
+++ src/003 - todomvc/views/Todos.jsx
@@ -4,35 +4,143 @@ import '../../../node_modules/todomvc-common/base.css'
 import '../../../node_modules/todomvc-app-css/index.css'
 
 class Todos extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isAll: false,
+      etitle: '',
+      todos: [
+        {
+          id: 1,
+          etitle: '写代码',
+          done: false
+        },
+        {
+          id: 2,
+          etitle: '听音乐',
+          done: true
+        }
+      ]
+    }
+
+    this.focusFef = React.createRef()
+  }
+
+  componentDidMount() {
+    // 自定义光标聚焦
+    this.focusFef.current.focus()
+  }
+
+  // 输入最新的任务
+  onKeyUpHandle = (e) => {
+    if (e.keyCode === 13) {
+      let ids = this.state.todos.map(item => {
+        return item.id
+      })
+
+      // 获取 Id 的最大值
+      let maxId = Math.max.apply(null, ids) + 1
+
+      let newTodo = {
+        id: maxId,
+        etitle: this.state.etitle,
+        done: false
+      }
+
+      this.setState({
+        todos: [newTodo, ...this.state.todos],
+        etitle: ''
+      })
+    }
+  }
+
+  // 获取最新输入的任务
+  valueChange = (e) => {
+    this.setState({
+      etitle: e.target.value
+    })
+  }
+
+  // 删除任务
+  deleteTodo = (id) => {
+    let todos = [...this.state.todos]
+    let index = todos.findIndex(item => {
+      return item.id === id
+    })
+
+    // 根据索引删除数组的元素
+    todos.splice(index, 1)
+    this.setState({
+      todos
+    })
+  }
+
+  // 全部标记已经完成
+  handleCheckAll = () => {
+    this.setState((state) => {
+      return {
+        isAll: !state.isAll
+      }
+    }, () => {
+      let todos = [...this.state.todos]
+      todos.forEach(item => {
+        item.done = this.state.isAll
+      })
+
+      this.setState({
+        todos
+      })
+    })
+  }
+
+  // 切换状态
+  handleCheck = (id) => {
+    let todos = [...this.state.todos]
+    todos.some(item => {
+      if (item.id === id) {
+        item.done = !item.done
+        // 终止遍历
+        return true
+      }
+      return false
+    })
+
+    this.setState({
+      todos
+    })
+  }
+
+  // 渲染 todolist 列表
+  todoTags = () => {
+    return this.state.todos.map(item => (
+      <li key={item.id} className={item.done ? 'completed' : ''}>
+        <div className="view">
+          <input onChange={() => this.handleCheck(item.id)} checked={item.done} className="toggle" type="checkbox" />
+          <label>{item.etitle}</label>
+          <button onClick={() => this.deleteTodo(item.id)} className="destroy"></button>
+        </div>
+        <input className="edit" />
+      </li>
+    ))
+  }
+
   render() {
+    const { etitle } = this.state
     return (
       <div>
         <section className="todoapp">
           <header className="header">
             <h1>todos</h1>
-            <input className="new-todo" placeholder="你想做什么 ?" autoFocus />
+            <input value={etitle} onChange={this.valueChange} onKeyUp={this.onKeyUpHandle} className="new-todo" placeholder="你想做什么 ?" ref={this.focusFef} />
           </header>
 
           <section className="main">
             <input id="toggle-all" className="toggle-all" type="checkbox" />
-            <label htmlFor="toggle-all">Mark all as complete</label>
+            <label onClick={this.handleCheckAll} htmlFor="toggle-all">全部标记为完成</label>
             <ul className="todo-list">
-              <li className="completed">
-                <div className="view">
-                  <input className="toggle" type="checkbox" />
-                  <label>吃饭</label>
-                  <button className="destroy"></button>
-                </div>
-                <input className="edit" />
-              </li>
-              <li>
-                <div className="view">
-                  <input className="toggle" type="checkbox" />
-                  <label>喝水</label>
-                  <button className="destroy"></button>
-                </div>
-                <input className="edit"  />
-              </li>
+              {this.todoTags()}
             </ul>
           </section>
 
